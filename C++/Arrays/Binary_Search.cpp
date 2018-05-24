bfc05b01eb993aec0cec657600708a7069d3f7c2
AKSHAY JAIN <akshayjain0297@gmail.com>
2018-05-24 09:06:01 +0530

    #4

---
+++ C++/Arrays/Binary_Search.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+// A recursive binary search function. It returns
+// location of x in given array arr[l..r] is present,
+// otherwise -1
+int binarySearch(int arr[], int l, int r, int x)
+{
+   if (r >= l)
+   {
+        int mid = l + (r - l)/2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid-1, x);
+        return binarySearch(arr, mid+1, r, x);
+   }
+   return -1;
+}
+
+int main(void)
+{
+   int arr[] = { 1 , 2, 3, 4, 5,6,7,8,9,10};
+   int n = sizeof(arr)/ sizeof(arr[0]);
+   int x;
+   cout<<"Enter Key :";
+   cin >> x;
+   int result = binarySearch(arr, 0, n-1, x);
+   if(result == -1)
+    printf("Element is not present in array");
+   else printf("Element is present at index %d",result);
+   return 0;
+}
